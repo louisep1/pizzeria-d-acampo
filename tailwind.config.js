2ee4cbe6004b953e08ea87f4ff0eb5beb8362da2
louisep1 <89385533+louisep1@users.noreply.github.com>
2022-10-13 16:16:05 +0900

    styling hero and nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-blue': '#82ddf0',
+        'med-blue': '#5296a5',
+        'dark-sand': '#f6c28b',
+        'light-sand': '#fcd7ad',
+        brown: '#a57548',
+        'yellow-beige': '#e1c591',
+        'dark-blue': '#1b3159',
+        'clear-black': 'rgba(0, 0, 0, 0.25)',
+      },
+      backgroundImage: {
+        'hero-pattern': "url('/src/assets/hero/pizza-oven.jpg')",
+      },
+    },
+    fontFamily: {
+      serif: 'Roboto Slab, serif',
+      sans: 'Lato, sans-serif',
+    },
   },
   plugins: [],
 }
