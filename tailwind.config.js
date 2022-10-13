c05ce7809b83b002ae39a8db1cf61812ddee510c
louisep1 <89385533+louisep1@users.noreply.github.com>
2022-10-13 20:52:46 +0900

    starting last section

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,9 +16,13 @@ module.exports = {
         'yellow-beige': '#e1c591',
         'dark-blue': '#1b3159',
         'clear-black': 'rgba(0, 0, 0, 0.25)',
+        'clear-white': 'rgba(255, 255, 255, 0.1)',
       },
       backgroundImage: {
-        'hero-pattern': "url('/src/assets/hero/pizza-oven.jpg')",
+        'hero-1': "url('/src/assets/hero/pizza-oven.jpg')",
+        'hero-2': "url('/src/assets/hero/fire.jpg')",
+        'hero-3': "url('/src/assets/hero/cooked-pizza.jpg')",
+        restaurant: "url('/src/assets/background/restaurant.jpg')",
       },
     },
     fontFamily: {
